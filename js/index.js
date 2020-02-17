f30a71e1ed4dbf22e0fbc5870228828d53c542a5
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-02-16 21:16:55 -0500

    Updated header and body scroll event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+let funBusHeader = document.querySelector("h1")
+funBusHeader.addEventListener ("mouseenter", () => {
+    funBusHeader.style.fontFamily = "monospace"
+})
+
+let funBusHeader2 = document.querySelector("h1")
+funBusHeader2.addEventListener ("mouseleave", () => {
+    funBusHeader2.style.fontFamily = "Indie Flower, cursive"
+})
+
+let bodyScroll = document.querySelector("body")
+document.addEventListener ("scroll", () => {
+    bodyScroll.style.backgroundColor = "aliceBlue"
+    bodyScroll.style.color = "cornflowerBlue"
+})
