92b45e68acccb5ac456542eaff4812ff9ca3c6be
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-02-16 22:04:41 -0500

    Added event listeners to bus picture and nav bar

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
 // Your code goes here
 
 let funBusHeader = document.querySelector("h1")
-funBusHeader.addEventListener ("mouseenter", () => {
+funBusHeader.addEventListener("mouseenter", () => {
     funBusHeader.style.fontFamily = "monospace"
 })
 
 let funBusHeader2 = document.querySelector("h1")
-funBusHeader2.addEventListener ("mouseleave", () => {
+funBusHeader2.addEventListener("mouseleave", () => {
     funBusHeader2.style.fontFamily = "Indie Flower, cursive"
 })
 
 let bodyScroll = document.querySelector("body")
-document.addEventListener ("scroll", () => {
+document.addEventListener("scroll", () => {
     bodyScroll.style.backgroundColor = "aliceBlue"
     bodyScroll.style.color = "cornflowerBlue"
+})
+
+let navBarChange = document.querySelector("a")
+navBarChange.addEventListener("click", () => {
+    navBarChange.style.color = "pink"
+})
+
+let busPicture = document.querySelector(".main-navigation")
+busPicture.addEventListener("dblclick", () => {
+    busPicture.style.border = "8px dotted blue"
 })
