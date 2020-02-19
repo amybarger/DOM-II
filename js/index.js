6d9a2389ba896f9a1cb01e664a3250c25f24b892
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-02-18 19:21:18 -0500

    Add stopPropagation and preventDefault event listeners

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 
+// 10 EVENT LISTENERS
+
 let funBusHeader = document.querySelector("h1")
 funBusHeader.addEventListener("mouseenter", () => {
     funBusHeader.style.fontFamily = "monospace"
@@ -51,3 +53,24 @@ footerSelect.addEventListener("mouseover", () => {
     footerSelect.style.border = "2px solid pink"
 })
 
+// STOP PROPAGATION
+
+let logoBackground = document.querySelector(".main-navigation")
+logoBackground.addEventListener("click", () => {
+    logoBackground.style.backgroundColor = "yellow"
+})
+
+let navBackground = document.querySelector(".nav")
+navBackground.addEventListener("click", () => {
+    navBackground.style.border = "2px solid green"
+    navBackground.style.backgroundColor = "orange"
+    event.stopPropagation();
+})    
+
+// PREVENT DEFAULT
+
+let stopRefreshLink = document.querySelector(".nav");
+
+stopRefreshLink.addEventListener("click", () => {
+    event.preventDefault();
+})
